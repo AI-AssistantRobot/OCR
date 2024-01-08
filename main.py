d83066079bacf4b2047be97681225ae62127bf10
Jung DongGyu <jdg824@naver.com>
2024-01-08 15:23:43 +0900

    Update main.py
    현재화면에서만 추론가능하도록 수정 -> 파일 저장 X

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import easyocr
 import cv2
+import easyocr
 
 reader = easyocr.Reader(['ko', 'en'], gpu=False)
 
 cap = cv2.VideoCapture(0)
 
+title = ""  #OCR 추론 결과 문자열
+
 while True:
     ret, frame = cap.read()
 
-    text = reader.readtext(frame, detail=0)
+    cv2.imshow('title', frame)
+    key = cv2.waitKey(1) #화면 잠시 멈춤
 
-    cv2.imshow('텍스트 인식', frame)
-    print(text)
+    if key == ord('c'): #c 누르면 정지화면 -> c 대신 캡쳐 누르면 데이터 받을수 있게 진행
+        captured_text = reader.readtext(frame, detail=0)
+        title = "".join(captured_text) #문자열 저장
+        print("TITLE : ", title)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    elif key == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
